db6f39c0333eb89d7023c0f0c1b0cda8f9c67f0a
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-11-17 21:49:39 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-
 import Pagination from './pagination';
 
-const container = document.getElementById('pagination');
-//const pagination = new Pagination(container);
 
-let pagination = null;
+const API_KEY = '0556b87ba267edab76fd3e7e8d7e5097';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TREND_URL = 'https://api.themoviedb.org/3/trending/movie/week';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
+const ID_URL = 'https://api.themoviedb.org/3/movie/';
+const GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
 
-pagination = new Pagination(300, 10, container);
-      pagination.create();
--- src/pagination.js
+++ src/pagination.js
@@ -1,33 +1,52 @@
-import Tui from 'tui-pagination';
-import './pagination.css';
+import Pagination from 'tui-pagination';
+//import { getTrendMovies, searchMovies, setPage } from './fetchMoviesAPI';
+//import renderPage from './renderCard';
+//import smoothScrool from './smothScrool';
+//import getRefs from './get-refs';
 
-class Pagination {
-  constructor(totalItems, itemsPerPage, buttonHolderID) {
-    this.totalItems = totalItems;
-    this.itemsPerPage = itemsPerPage;
-    this.buttonHolderID = buttonHolderID;
-  }
+const refs = getRefs();
 
-  handler = null;
-  buttonHolder = null;
-
-  create() {
-    this.buttonHolder = document.getElementById(this.buttonHolderID);
-    this.handler = new Tui(this.buttonHolder, {
-      totalItems: this.totalItems,
-      itemsPerPage: this.itemsPerPage,
-      visiblePages: 5,
-      centerAlign: true,
+export default function renderPagination(result, query, value) {
+  const container = refs.pagBox;
+  const options = {
+    totalItems: result.total_results,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: result.page,
+    centerAlign: true,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+  };
+  const pagination = new Pagination(container, options);
+  if (query === 'search') {
+    pagination.on('afterMove', event => {
+      const currentPage = event.page;
+      setPage(currentPage);
+      searchMovies(value).then(r => renderPage(r.results));
+      smoothScrool(0, 400);
+    });
+  } else {
+    pagination.on('afterMove', event => {
+      const currentPage = event.page;
+      setPage(currentPage);
+      getTrendMovies();
+      smoothScrool(0, 400);
     });
   }
-
-  reset() {
-    if (this.handler){
-      this.handler = null;
-      this.buttonHolder.innerHTML = '';
-      this.buttonHolder = null;
-    }
-  }
 }
-
-export default Pagination;
