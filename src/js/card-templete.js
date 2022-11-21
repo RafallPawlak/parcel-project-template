91300553c03854103120c6d6b55d2f5374bdcaae
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-11-21 14:53:32 +0100

    a

--- src/js/card-templete.js
+++ src/js/card-templete.js
@@ -1,7 +1,3 @@
-
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
-
 const API_KEY_V3 = "bdba5342660bdd1dac5d09b885091a0c";
 const API_KEY_V4 = "";
 const API_URL = "https://api.themoviedb.org/3/";
@@ -9,6 +5,7 @@ const API_URL = "https://api.themoviedb.org/3/";
 const filmsListHtml = document.querySelector(".movieadd");
 
 let tempImageUrl = "";
+
 
 fetchApi();
 
@@ -19,8 +16,9 @@ async function fetchApi(){
         .then(film => {
     
           const filmDetailsHtml = document.querySelectorAll(".film-details");
+          //console.log("fetchApi -> fetchApiTrending.then here forEach start to get film details with fetchApiGetDetailsFilm methode");
           filmDetailsHtml.forEach(el => {
-        //  / fetchApiGetDetailsFilm(el)
+          fetchApiGetDetailsFilm(el)
       })
     })
   } catch (error) {
@@ -28,7 +26,7 @@ async function fetchApi(){
   }
 };
 
-
+// ------------------get image link methode---------------------
 async function fetchApiConfig(){
   try {
     const params = new URLSearchParams({
@@ -36,22 +34,27 @@ async function fetchApiConfig(){
     });
     const response = await fetch(API_URL + "configuration" + "?" + params);
     const config = await response.json();
+   // console.log("fetchApiConfig object content:", config);
     tempImageUrl = config.images.base_url + config.images.poster_sizes[3];
+   // console.log("function fetchApiConfig base url to get img:", tempImageUrl);
   } catch (error) {
     console.log("fetchApiConfig: ", error);
   }
 };
 
-
-async function fetchApiTrending(page){
+// -------------------get trending films-----------------------
+async function fetchApiTrending(pageN){
   try {
     const params = new URLSearchParams({
       api_key: API_KEY_V3,
-      page: page,
+      page: pageN,
     });
     const response = await fetch(API_URL + "trending/" + "movie/" + "day" + "?" + params);
     const film = await response.json();
+
     let imageLink = tempImageUrl + film.results[0].poster_path;
+    //console.log("fetchApiTrending completed link to img:", imageLink);
+   //console.log("fetchApiTrending forEach start to create HTML li>img tags");
     let filmItems = "";
     film.results.forEach(result => {
       filmItems+=`
@@ -68,40 +71,23 @@ async function fetchApiTrending(page){
     console.log("fetchApiTrending: ", error);
   }
 };
+// --------------------get genres and year methode----------------------
+async function fetchApiGetDetailsFilm(elHtml){
+  try {
+    const params = new URLSearchParams({
+      api_key: API_KEY_V3
+    });
+    const response = await fetch(API_URL + "movie/" + elHtml.dataset.film_id + "?" + params)
+    const filmDetails = await response.json();
 
-
-
-const container = document.getElementById('pagination');
-const options = { // below default value of options
-     totalItems: 1000,
-     itemsPerPage: 5,
-     visiblePages: 5,
-     page: 1,
-     centerAlign: false,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
+    //console.log("fetchApiGetDetailsFilm object content:", filmDetails);
+    let yearWithDate = new Date(filmDetails.release_date);
+    const genresArray = filmDetails.genres.map(genre => {return genre.name});
+    //console.log("genres array - result .map methode inside fetchApiGetDetailsFilm:", genresArray);
+    elHtml.innerHTML = `${genresArray.join(", ")} &#124; ${yearWithDate.getFullYear()}`
+  } catch (error) {
+    console.log("fetchApiGetDetailsFilm: ", error);
+  }
 };
-const pagination = new Pagination(container, options);
 
-pagination.on('afterMove', (event) => {
-  const currentPage = event.page;
-  console.log(currentPage);
-  fetchApiTrending(currentPage);
-});
 
